06daf2c20295f02ad1b899bd01fa7bcb5c7d7f09
Krzysztof Cieślak <krzysztof_cieslak@windowslive.com>
2019-11-18 19:10:33 +0000

    Improve sample

---
+++ README.md
@@ -0,0 +1,9 @@
+# F# GitPod sample
+
+Sample for using GitPod with F# based on .Net Core 3.0. It shows both simple project (command line app) and script file.
+The important parts are:
+* `.docker/Dockerfile` using .Net Core 3.0
+* `.vscode/settings.json` enabling .Net Core F# Interactive (`dotnet fsi`)
+* `.vscode/tasks.json` defining build task for the application
+
+[![Open in Gitpod](https://gitpod.io/button/open-in-gitpod.svg)](https://gitpod.io#https://github.com/Krzysztof-Cieslak/GitPodSample)
--- test.fsx
+++ test.fsx
@@ -1,1 +1,11 @@
+//ignore the fact that there are some errors reported on the first line, it's mostly cosmetical, and will go away in the next release of Ionide
+
+//It seems that GitPod has some problems with the support for the VSCode's Terminal API that we use for the FSI intgration in Ionide. 
+//Commands like "Start FSI", or "Send line to FSI" are not working correctly, need to investigate more. 
+//It means that usuability of the .fsx files in GitPod is limited at the moment.
+
 let t = 1
+
+let z = System.DateTime.Now
+
+printfn "Z is %A" z
